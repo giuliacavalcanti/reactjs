69e27c07c29144cf841d72efe3a314137c30db42
Giulia Cavalcanti <giulia_cavalcanti@apple.com>
2020-03-18 20:52:19 -0300

    Finishing day one

--- src/App.js
+++ src/App.js
@@ -3,12 +3,45 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:'Giulia', age: 23 },
+      { name:'Gabriela', age: 25 },
+      { name: 'Beatriz', age: 22 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked');
+    this.setState( {
+      persons: [
+        { name: newName, age: 23 },
+        { name:'Gabriela', age: 25 },
+        { name: 'Beatriz Stefano', age: 23 }
+      ]
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name:'Giulia Falcão', age: 23 },
+        { name: event.target.value, age: 25 },
+        { name: 'Beatriz Stefano', age: 23 }
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
       <h1> Hi, I'm a React app </h1>
       <p>I'm learning ReactJS!</p>
-      <Person name='Giulia' age='24'/>
+      
+      <button onClick={this.switchNameHandler}> Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChangedHandler}>My hobbie: Coding</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Gabriela Alencar')}>My hobbie: Cundling alceu</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../Person/Person.css';
 
 const person = (props) => {
-    return <p>I'm a {props.name} and I'm {props.age} years old!</p>
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed}/>
+        </div>
+    )
 }
 
 export default person;
